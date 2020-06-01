1c7ceb3cdf79eb6f284af432a6dbc095c5934090
sidv <sidharthv96@gmail.com>
2020-06-01 23:11:08 +0530

    render

--- tambola/static/game.js
+++ tambola/static/game.js
@@ -19,31 +19,28 @@ showWinners();
 async function showWinners() {
   const tickets = await getTickets();
   const markedNums = await getMarkedNums();
-  const markedTickets = tickets.map(ticket => markTicket(ticket, markedNums));
+  const markedTickets = tickets.map((ticket) => markTicket(ticket, markedNums));
 
   const rootDiv = document.getElementById('win_patterns');
-  rootDiv.innerHTML = "";
-  for(let i = 0; i < winPatterns.length; i++) {
-    const pattern = winPatterns[i];
-    rootDiv.innerHTML += ('<div>');
-    rootDiv.innerHTML += ('<h3>' + pattern.name + '</h3>');
-    const wonThisPattern = markedTickets.filter(pattern.validator);
-    rootDiv.innerHTML += ('<ul>');
-    for(let j = 0; j < wonThisPattern.length; j++) {
-      rootDiv.innerHTML += ('<li>' + wonThisPattern[j].name + '</li>');
-    }
-    rootDiv.innerHTML += ('</ul>');
-    rootDiv.innerHTML += ('</div>');
-  }
+  rootDiv.innerHTML = '';
+  winPatterns.forEach((pattern) => {
+    rootDiv.innerHTML += `<div>
+      <h3>${pattern.name}</h3>
+      <ul>
+      ${markedTickets
+        .filter(pattern.validator)
+        .map((winningPattern) => `<li>${winningPattern.name}</li>`)
+        .join('')}
+      </ul>
+    </div>`;
+  });
 }
 
 function markTicket(ticket, markedNums) {
-  for(let i = 0; i < ticket.data.length; i++) {
-    for(let j = 0; j < ticket.data[i].length; j++) {
-      for(let k = 0; k < markedNums.length; k++) {
-        if(ticket.data[i][j] === markedNums[k]) {
-          ticket.data[i][j] = "x";
-        }
+  for (let i = 0; i < ticket.data.length; i++) {
+    for (let j = 0; j < ticket.data[i].length; j++) {
+      if (markedNums.includes(ticket.data[i][j])) {
+        ticket.data[i][j] = 'x';
       }
     }
   }
@@ -51,24 +48,24 @@ function markTicket(ticket, markedNums) {
 }
 
 async function getMarkedNums() {
-  const response = await fetch("/games/");
+  const response = await fetch('/games/');
 
-  if (response.ok) { 
+  if (response.ok) {
     const json = await response.json();
-    return json.numbers.split(",");
+    return json.numbers.split(',');
   } else {
-    console.log("HTTP-Error: " + response.status);
+    console.log('HTTP-Error: ' + response.status);
   }
 }
 
 async function getTickets() {
-  let response = await fetch("/games/tickets/");
+  let response = await fetch('/games/tickets/');
 
-  if (response.ok) { 
+  if (response.ok) {
     let json = await response.json();
     return json.map(processTicket);
   } else {
-    console.log("HTTP-Error: " + response.status);
+    console.log('HTTP-Error: ' + response.status);
   }
 }
 
